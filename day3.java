fcc8f442450015f547a57b396f16d96154950ece
Adrien Abbey <abbey.12@wright.edu>
2024-01-01 04:33:57 -0500

    now correctly identifying integer locations

--- day3.java
+++ day3.java
@@ -5,6 +5,7 @@
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Scanner;
 
 class day3 {
@@ -39,17 +40,61 @@ class day3 {
         }
 
         // FIXME: Test code:
-        System.out.println(" Input rows: " + matrixRows);
-        System.out.println(" Input columns: " + matrixCols);
+        // System.out.println(" Input rows: " + matrixRows);
+        // System.out.println(" Input columns: " + matrixCols);
+        // for (int i = 0; i < matrixRows; i++) {
+        // String line = new String();
+        // for (int j = 0; j < matrixCols; j++) {
+        // line = line + "" + inputMatrix[i][j];
+        // }
+        // System.out.println(line);
+        // }
+
+        // Start scanning for integers:
         for (int i = 0; i < matrixRows; i++) {
-            String line = new String();
             for (int j = 0; j < matrixCols; j++) {
-                line = line + "" + inputMatrix[i][j];
+                int[] foundInteger = findNumber(inputMatrix, i, j, matrixRows, matrixCols);
+                int[] notFoundMatrix = { -1, -1 };
+                if (!Arrays.equals(foundInteger, notFoundMatrix)) {
+                    // Integer found. Skip to next column for testing:
+                    j = foundInteger[1] + 1;
+
+                    // FIXME: Print it:
+                    System.out.println(" Found integer: row " + i + ", columns " + Arrays.toString(foundInteger));
+                }
             }
-            System.out.println(line);
         }
 
         // Close the scanner before exiting:
         inputScanner.close();
+    }
+
+    public static int[] findNumber(char[][] charMatrix, int row, int column, int maxRow, int maxCol) {
+        // Searches the given char matrix at the given row and column for an
+        // integer. If one is found, it returns the starting and ending column
+        // as an integer matrix. If no integer is found, returns [-1][-1].
+
+        // If the given row and column starts with an integer:
+        if (Character.isDigit(charMatrix[row][column])) {
+            // Then start searching from the current position until no more
+            // integers are found:
+            int finalColumn = column;
+            for (int i = column + 1; i <= maxCol; i++) {
+                if (Character.isDigit(charMatrix[row][i])) {
+                    finalColumn = i;
+                } else {
+                    break;
+                }
+            }
+
+            // Found the start and end column of the integer; return that:
+            int returnArray[] = { column, finalColumn };
+            return returnArray;
+        } else {
+
+            // Nothing found, return negative values:
+            int returnArray[] = { -1, -1 };
+            return returnArray;
+        }
     }
 }
